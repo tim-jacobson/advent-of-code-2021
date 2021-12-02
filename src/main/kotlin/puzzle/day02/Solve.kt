cad759e55937dbb739c9a1a6184307402c89307d
Timothy Jacobson <tim.jacobson@bigtelematics.com>
2021-12-02 10:22:42 +0000

    [Solution - Day 02 - Part 2]

--- src/main/kotlin/puzzle/day02/Solve.kt
+++ src/main/kotlin/puzzle/day02/Solve.kt
@@ -14,14 +14,14 @@ forward 2""".trim()
 
     val output = inputFile.split('\n')
         .map { line -> line.split(' ').let { it.first() to it.last().toInt() } }
-        .fold(0 to 0) { acc, pair ->
+        .fold((0 to 0) to 0) { (acc, a), pair ->
             when (pair.first) {
-                "forward" -> acc.copy(first = acc.first + pair.second)
-                "down" -> acc.copy(second = acc.second + pair.second)
-                "up" -> acc.copy(second = acc.second - pair.second)
+                "forward" -> acc.copy(first = acc.first + pair.second, second = acc.second + (a * pair.second)) to a
+                "down" -> acc to (a + pair.second)
+                "up" -> acc to (a - pair.second)
                 else -> throw Exception("Unknown direction")
             }
-        }.let { it.first * it.second }
+        }.let { it.first.first * it.first.second }
 
     println(output)
 }
